160b77f11bce667663673372d7098381eccaf1a0
Grant Nickles <gnickles@wisc.edu>
2023-11-14 18:35:31 -0600

    google tag debugging

--- CreateWebsite.py
+++ CreateWebsite.py
@@ -238,7 +238,33 @@ def CleanTable(labInfo):
 
     return labInfo #returning the cleaned table
 
+### Adding in the google tag
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R7JFW1HZ2R"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
+            gtag('config', 'G-R7JFW1HZ2R');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
 
 if __name__ == '__main__':
     app.run(debug=True)
